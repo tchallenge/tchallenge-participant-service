44613b257f108e0cd23d4d63050f8548b6c8096e
onlythenaive <ilya.gubarev@gmail.com>
2018-01-24 13:26:49 +0300

    Event uses Maturity and Specialization

--- source/src/main/java/ru/tchallenge/participant/service/domain/event/Event.java
+++ source/src/main/java/ru/tchallenge/participant/service/domain/event/Event.java
@@ -2,6 +2,8 @@ package ru.tchallenge.participant.service.domain.event;
 
 import lombok.Builder;
 import lombok.Data;
+import ru.tchallenge.participant.service.domain.maturity.Maturity;
+import ru.tchallenge.participant.service.domain.specialization.Specialization;
 import ru.tchallenge.participant.service.utility.data.IdAware;
 
 import java.time.Instant;
@@ -19,7 +21,7 @@ public final class Event implements IdAware {
     private final Instant validFrom;
     private final Instant validUntil;
     private final Collection<String> notifications;
-    private final Collection<String> maturities;
-    private final Collection<String> specializations;
+    private final Collection<Maturity> maturities;
+    private final Collection<Specialization> specializations;
     private final String status;
 }
