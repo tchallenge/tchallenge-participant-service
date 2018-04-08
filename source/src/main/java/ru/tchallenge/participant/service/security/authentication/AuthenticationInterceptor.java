2f821d8646ffbca8c01913b1eeb2199d9de5d5b9
onlythenaive <ilya.gubarev@gmail.com>
2018-04-08 13:04:26 +0300

    SimpleEndpointSignature added

--- source/src/main/java/ru/tchallenge/participant/service/security/authentication/AuthenticationInterceptor.java
+++ source/src/main/java/ru/tchallenge/participant/service/security/authentication/AuthenticationInterceptor.java
@@ -2,9 +2,6 @@ package ru.tchallenge.participant.service.security.authentication;
 
 import java.util.Set;
 
-import lombok.Builder;
-import lombok.Data;
-
 import spark.Request;
 import spark.Response;
 
@@ -12,13 +9,14 @@ import com.google.common.collect.Sets;
 
 import ru.tchallenge.participant.service.security.token.SecurityToken;
 import ru.tchallenge.participant.service.security.token.SecurityTokenContext;
+import ru.tchallenge.participant.service.utility.http.SimpleEndpointSignature;
 import static ru.tchallenge.participant.service.utility.serialization.Json.body;
 
 public final class AuthenticationInterceptor {
 
     public static final AuthenticationInterceptor INSTANCE = new AuthenticationInterceptor();
 
-    private final Set<EndpointSignature> authorizationByInvoice;
+    private final Set<SimpleEndpointSignature> authorizationByInvoice;
     private final String authorizationHeaderName = "Authorization";
     private final String authorizationHeaderPrefix = "BEARER ";
     private final AuthenticationContext authenticationContext = AuthenticationContextBean.INSTANCE;
@@ -26,7 +24,7 @@ public final class AuthenticationInterceptor {
     private final SecurityTokenContext securityTokenContext = SecurityTokenContext.INSTANCE;
 
     private AuthenticationInterceptor() {
-        authorizationByInvoice = Sets.newHashSet(new EndpointSignature("POST", "/security/tokens/"));
+        authorizationByInvoice = Sets.newHashSet(SimpleEndpointSignature.builder().method("POST").uri("/security/tokens/").build());
     }
 
     public void before(final Request request, final Response response) {
@@ -54,7 +52,7 @@ public final class AuthenticationInterceptor {
     }
 
     private boolean shouldAuthenticateByInvoice(final Request request) {
-        return authorizationByInvoice.contains(EndpointSignature.of(request));
+        return authorizationByInvoice.contains(SimpleEndpointSignature.of(request));
     }
 
     private Authentication authenticateByInvoice(final Request request) {
@@ -84,17 +82,5 @@ public final class AuthenticationInterceptor {
 
     private RuntimeException authenticationMethodUnknown() {
         return new RuntimeException("Authentication method is not supported");
-    }
-
-    @Data
-    @Builder
-    private static final class EndpointSignature {
-
-        static EndpointSignature of(final Request request) {
-            return EndpointSignature.builder().method(request.requestMethod()).uri(request.uri()).build();
-        }
-
-        private final String method;
-        private final String uri;
     }
 }
