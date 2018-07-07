9ae438ab7a1ca33cb80c452e56e9bb390dbba782
onlythenaive <ilya.gubarev@gmail.com>
2018-07-07 16:25:14 +0300

    datastore scripts + demo data added

---
+++ datastorage/tchallenge-pilot-datastore-export.cmd
@@ -0,0 +1,5 @@
+mongoexport --db tchallenge-pilot --collection accounts --out ./data/exported/tchallenge-pilot-accounts.json
+mongoexport --db tchallenge-pilot --collection events --out ./data/exported/tchallenge-pilot-events.json
+mongoexport --db tchallenge-pilot --collection problems --out ./data/exported/tchallenge-pilot-problems.json
+mongoexport --db tchallenge-pilot --collection specializations --out ./data/exported/tchallenge-pilot-specializations.json
+mongoexport --db tchallenge-pilot --collection workbooks --out ./data/exported/tchallenge-pilot-workbooks.json
---
+++ datastorage/tchallenge-pilot-datastore-import.cmd
@@ -0,0 +1,5 @@
+mongoimport --db tchallenge-pilot --collection accounts --file ./data/demo/tchallenge-pilot-accounts.json
+mongoimport --db tchallenge-pilot --collection events --file ./data/demo/tchallenge-pilot-events.json
+mongoimport --db tchallenge-pilot --collection problems --file ./data/demo/tchallenge-pilot-problems.json
+mongoimport --db tchallenge-pilot --collection specializations --file ./data/demo/tchallenge-pilot-specializations.json
+mongoimport --db tchallenge-pilot --collection workbooks --file ./data/demo/tchallenge-pilot-workbooks.json
